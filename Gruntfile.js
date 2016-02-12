7dff0b221093ab49bd70e565ecfe07a9296dd131
Mihajlo <mihajlo.stevanovic@parnuwks01-103.nurun.com>
2016-02-12 17:58:58 +0100

    grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,61 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
+    
+    sass: {                              // Task
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        sourcemap: 'none'
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      dist: {                            // Target
+        files: [{                         // Dictionary of files
+          expand: true,
+          cwd: 'app/scss/',
+          src: ['*.scss'],
+          dest: 'app/styles/',
+          ext: '.css'
+        }]
+      }
+    },
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      sass: {
+        files: ['**/*.scss'],
+        tasks: ['sass']
+      },
+      scripts: {
+        files: ['**/*.js']
+      },
+      html: {
+        files: ['**/*.html']
+      }
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : [
+            '**/*.css',
+            '**/*.html',
+            '**/*.js'
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: 'app/'
+        }
       }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Load the plugin that provides the all tasks.
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', [
+    'sass',
+    'browserSync',
+    'watch'
+  ]);
 
 };
